f3bef1dd4edb9a62e10517074bea06a70a437773
Brian Sheppard <sheppardco@aol.com>
2017-04-27 18:43:50 -0400

    Use the movepicker stage as a condition in LMR.
    Bench: 5957270

--- src/search.cpp
+++ src/search.cpp
@@ -996,6 +996,12 @@ moves_loop: // When in check search starts from here
               else if (ss->history < 0 && (ss-1)->history > 0)
                   r += ONE_PLY;
 
+			  auto stage = mp.Stage();
+			  if (stage == GOOD_CAPTURES || stage == KILLERS || stage == COUNTERMOVE)
+			  {
+				  r -= ONE_PLY;
+			  }
+
               // Decrease/increase reduction for moves with a good/bad history
               r = std::max(DEPTH_ZERO, (r / ONE_PLY - ss->history / 20000) * ONE_PLY);
           }
