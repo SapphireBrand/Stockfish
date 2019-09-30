61cf730f4ac39e0e4d8bcd4f05fd885651b22c13
Brian Sheppard <brian.sheppard@toasttab.com>
2019-09-30 17:44:41 -0400

    Try the deeper deprecation when the eval is lower. bench: 3493015

--- src/search.cpp
+++ src/search.cpp
@@ -1139,7 +1139,7 @@ moves_loop: // When in check, search starts from here
                   r += ONE_PLY;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-              r -= ss->statScore / 16384 * ONE_PLY;
+              r -= ss->statScore / (eval > 50 ? 16384 : 14884) * ONE_PLY;
           }
 
           Depth d = clamp(newDepth - r, ONE_PLY, newDepth);
