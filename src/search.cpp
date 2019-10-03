723b4719ff8cb554d784295f4cd33e47a743c045
Brian Sheppard <brian.sheppard@toasttab.com>
2019-10-03 17:38:11 -0400

    stats constant 14884 if retreat bench 3999319

--- src/search.cpp
+++ src/search.cpp
@@ -1138,8 +1138,13 @@ moves_loop: // When in check, search starts from here
               else if ((ss-1)->statScore >= -117 && ss->statScore < -144)
                   r += ONE_PLY;
 
+              // Reduce depth if the move returns a piece to the square it occupied last turn. 
+              auto divisor = !inCheck && !givesCheck && to_sq(move) == from_sq((ss - 2)->currentMove) && from_sq(move) == to_sq((ss - 2)->currentMove)
+                ? 14884
+                : 16384;
+
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-              r -= ss->statScore / 16384 * ONE_PLY;
+              r -= ss->statScore / divisor * ONE_PLY;
           }
 
           Depth d = clamp(newDepth - r, ONE_PLY, newDepth);
