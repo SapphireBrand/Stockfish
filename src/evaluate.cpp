cd29159207196f44c065090f54503c476386b634
Brian Sheppard <sheppardco@aol.com>
2019-10-08 15:22:14 -0400

    15 * (int(eg) * int(mg) < 0) bench 3707792

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -723,6 +723,7 @@ namespace {
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
+                    + 15 * (int(eg) * int(mg) < 0)
                     -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
