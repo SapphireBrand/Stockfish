8fa5d752c649d34b08394cb94f113015ff25c86d
Brian Sheppard <sheppardco@aol.com>
2019-10-22 17:37:18 -0400

    + 10 * notBlocked                     -113 ; bench 5174113

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -716,6 +716,9 @@ namespace {
                            &&  outflanking < 0
                            && !pawnsOnBothFlanks;
 
+    auto whitePawnsUp = shift<NORTH>(pos.pieces(WHITE, PAWN));
+    bool notBlocked = whitePawnsUp ^ pos.pieces(BLACK, PAWN);
+
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
@@ -723,7 +726,8 @@ namespace {
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
-                    -103 ;
+                    + 10 * notBlocked
+                    -113 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
